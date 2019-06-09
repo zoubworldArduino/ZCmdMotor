e959d5bfaf9b22938f863e1124c9176caa6bf605
zoub <zoubata@yahoo.fr>
2019-06-09 14:37:51 +0200

    fix indentation

--- ZCmdMotor.cpp
+++ ZCmdMotor.cpp
@@ -14,61 +14,61 @@
 
 void CMDMOTOR::setSerialDebug(HardwareSerial * mySerialDebug)
 {
-  SerialDebug=mySerialDebug;
+	SerialDebug=mySerialDebug;
 }
 int CMDMOTOR::getPoint()
 {
-  return point;
+	return point;
 }
 void CMDMOTOR::setPoint(int mypoint) {
 
-   enabled=true;
-  
+	enabled=true;
+
 #ifdef SMOOTH
-  newPoint = mypoint;
-  if ((newPoint - point) > 0) {
-    point = point + MIN(dpointMax, (newPoint - point));
-  } else {
-    point = point - MIN(dpointMax, (point - newPoint));
-  }
+	newPoint = mypoint;
+	if ((newPoint - point) > 0) {
+		point = point + MIN(dpointMax, (newPoint - point));
+	} else {
+		point = point - MIN(dpointMax, (point - newPoint));
+	}
 #else
-  point=mypoint;
+	point=mypoint;
 #endif
 }
 void CMDMOTOR::Callback(int newValue) {
-  SerialDebug->print("D:");
-  SerialDebug->print(newValue, HEX);
-  SerialDebug->print("\r\n");
+	SerialDebug->print("D:");
+	SerialDebug->print(newValue, HEX);
+	SerialDebug->print("\r\n");
 }
 
 
 
 void CMDMOTOR::AutoTuneHelper(boolean start)
 {
-  if(start)
-    ATuneModeRemember = pid->GetMode();
-  else
-    pid->SetMode(ATuneModeRemember);
+	if(start)
+		ATuneModeRemember = pid->GetMode();
+	else
+		pid->SetMode(ATuneModeRemember);
 }
 
 void CMDMOTOR::changeAutoTune()
 {
- if(!tuning)
-  {
-    //Set the output to the desired starting frequency.
-    Output=aTuneStartValue;
-    aTune->SetNoiseBand(aTuneNoise);
-    aTune->SetOutputStep(aTuneStep);
-    aTune->SetLookbackSec((int)aTuneLookBack);
-    AutoTuneHelper(true);
-    tuning = true;
-  }
-  else
-  { //cancel autotune
-    aTune->Cancel();
-    tuning = false;
-    AutoTuneHelper(false);
-  }
+	if(!tuning)
+	{
+		//Set the output to the desired starting frequency.
+		Output=aTuneStartValue;
+		aTune->SetNoiseBand(aTuneNoise);
+		aTune->SetOutputStep(aTuneStep);
+		aTune->SetLookbackSec((int)aTuneLookBack);
+		AutoTuneHelper(true);
+		tuning = true;
+	}
+	else
+	{ //cancel autotune
+		aTune->Cancel();
+		tuning = false;
+		AutoTuneHelper(false);
+	}
 }
 
 /*Constructor (...)*********************************************************
@@ -77,117 +77,132 @@ void CMDMOTOR::changeAutoTune()
  ***************************************************************************/
 CMDMOTOR::CMDMOTOR(int INCA, int INCB, int MP, int MM) {
 #ifdef ROS_USED 
-    nh=0;
+	nh=0;
 #endif
- Kp = 4000.0/371029.0;// 10% of error
- Ki =4000.0/371029.0*100;//;// 4000.0/371029.0;//30;//0.05
- Kd =Kp/1000;//0.0005; //0.0005
- olddirection = 1;
- direction = -1;
- dpointMax = 50;
-    point=0; Input=1; Output=0;
- pid = new PID(&Input, &Output, &point, Kp, Ki, Kd, DIRECT);
-setRefreshRateUs(20000);
-    assert(pid!=0);// heap issue.
-// pid->SetSampleTime(10);
- aTune  = new PID_ATune(&Input, &Output);
-  assert(aTune!=0);// heap issue.
- tuning = false;
- enabled=false;
- aTuneStep=50, aTuneNoise=1, aTuneStartValue=100;
-  aTuneLookBack=20;
-SerialDebug=0;
- ATuneModeRemember=2;
-  /*
+	Kp = 4000.0/371029.0;// 10% of error
+	Ki =4000.0/371029.0*10;//;// 4000.0/371029.0;//30;//0.05
+	Kd =Kp/1000;//0.0005; //0.0005
+	olddirection = 1;
+	direction = -1;
+	dpointMax = 50;
+	point=0; Input=1; Output=0;
+	pid = new PID(&Input, &Output, &point, Kp, Ki, Kd, DIRECT);
+	setRefreshRateUs(20000);
+	assert(pid!=0);// heap issue.
+	// pid->SetSampleTime(10);
+	aTune  = new PID_ATune(&Input, &Output);
+	assert(aTune!=0);// heap issue.
+	tuning = false;
+	enabled=false;
+	aTuneStep=50, aTuneNoise=1, aTuneStartValue=100;
+	aTuneLookBack=20;
+	SerialDebug=0;
+	ATuneModeRemember=2;
+	/*
  if (!SerialDebug->enabled())
  {
  SerialDebug->begin(9600);  //1000000 , 115200
  }*/
-  if (SerialDebug) {
-    SerialDebug->print("CMDMOTOR(");
-    SerialDebug->print((signed int) INCA);
-    SerialDebug->print(",");
-    SerialDebug->print((signed int) INCA);
-    SerialDebug->print(",");
-    SerialDebug->print((signed int) MP);
-    SerialDebug->print(",");
-    SerialDebug->print((signed int) MM);
-    SerialDebug->print(") : ");
-  }
-   setPin(INCA, INCB, MP, MM);
+	if (SerialDebug) {
+		SerialDebug->print("CMDMOTOR(");
+		SerialDebug->print((signed int) INCA);
+		SerialDebug->print(",");
+		SerialDebug->print((signed int) INCA);
+		SerialDebug->print(",");
+		SerialDebug->print((signed int) MP);
+		SerialDebug->print(",");
+		SerialDebug->print((signed int) MM);
+		SerialDebug->print(") : ");
+	}
+	setPin(INCA, INCB, MP, MM);
 
 }
-	/** setup the refresh rate of the topic
-	*/
+/** setup the refresh rate of the topic
+ */
 void CMDMOTOR::setRefreshRateUs(uint32_t intervalTime //!< duration between 2 topic in Micro Seconde
-  )
-  {
-   if(getEncoder()!=0) 
-     getEncoder()->setRefreshRateUs(intervalTime);
-    if(pid!=0) 
-      pid->SetSampleTime(intervalTime/1000);
-  }
+)
+{
+	if(getEncoder()!=0) 
+		getEncoder()->setRefreshRateUs(intervalTime);
+	if(pid!=0) 
+		pid->SetSampleTime(intervalTime/1000);
+}
 void CMDMOTOR::setPin(int INCA, int INCB, int MP, int MM) 
 {
-   setPin( INCA,  INCB,  MP,  MM, -1) ;
+	setPin( INCA,  INCB,  MP,  MM, -1) ;
 }
 void CMDMOTOR::setPin(int INCA, int INCB, int MP, int MM, int EN) 
 {
-    pinINCA = INCA;
-  pinINCB = INCB;
-  pinMP = MP;
-  pinMM = MM;
-  pinEN = EN;
+	pinINCA = INCA;
+	pinINCB = INCB;
+	pinMP = MP;
+	pinMM = MM;
+	pinEN = EN;
 
-  pinMode(pinMP, OUTPUT);
-  pinMode(pinMM, OUTPUT);
-  digitalWrite(pinMP, LOW);
-  digitalWrite(pinMM, LOW);
-  
+	pinMode(pinMP, OUTPUT);
+	pinMode(pinMM, OUTPUT);
+	digitalWrite(pinMP, LOW);
+	digitalWrite(pinMM, LOW);
 
-  pinMode(pinEN, OUTPUT);
-  digitalWrite(pinEN, HIGH); 
 
+	pinMode(pinEN, OUTPUT);
+	digitalWrite(pinEN, HIGH); 
+	if(encoder==0)
+	{
+		if  (!((pinINCA==-1) &&(pinINCB==-1) ))
+		{
+			encoder = new ZEncoder(pinINCA, pinINCB, QUARTER, NULL); //FULL, QUARTER
+			assert(encoder!=0);// heap issue.
+		}
+	}
+	else
+	{
+		/*
+  encoder->setPin( INCA,  INCB);
+		 */
+	}
 }
 
 ZEncoder * CMDMOTOR::getEncoder() {
-  return encoder;
+	return encoder;
 }
 PID * CMDMOTOR::getPID() {
-  return pid;
+	return pid;
 }
 void CMDMOTOR::setup() {
-  if (SerialDebug) {
+	if (SerialDebug) {
 
-    SerialDebug->print("CMDMOTOR.setup(");
-    SerialDebug->print((signed int) pinINCA);
-    SerialDebug->print(",");
-    SerialDebug->print((signed int) pinINCB);
-    SerialDebug->print(",");
-    SerialDebug->print((signed int) pinMP);
-    SerialDebug->print(",");
-    SerialDebug->print((signed int) pinMM);
-    SerialDebug->print(") : ");
-  }
+		SerialDebug->print("CMDMOTOR.setup(");
+		SerialDebug->print((signed int) pinINCA);
+		SerialDebug->print(",");
+		SerialDebug->print((signed int) pinINCB);
+		SerialDebug->print(",");
+		SerialDebug->print((signed int) pinMP);
+		SerialDebug->print(",");
+		SerialDebug->print((signed int) pinMM);
+		SerialDebug->print(") : ");
+	}
+	if (!((pinINCA==-1) &&(pinINCB==-1) ))
+	{
+		encoder = new ZEncoder(pinINCA, pinINCB, QUARTER, NULL); //FULL, QUARTER
+		assert(encoder!=0);// heap issue.
+	}
+	//turn the PID on
+	pid->SetMode(MANUAL);
 
-  encoder = new ZEncoder(pinINCA, pinINCB, QUARTER, NULL); //FULL, QUARTER
-  assert(encoder!=0);// heap issue.
-  //turn the PID on
-  pid->SetMode(MANUAL);
+	//pid->SetOutputLimits(-255, 255);
 
-  //pid->SetOutputLimits(-255, 255);
-  
-pid->SetOutputLimits(-14096,14095);
-  pid->SetSampleTime(10);  //10ms
-  pid->SetMode(AUTOMATIC);
-//  pid.SetControllerDirection(REVERSE );
- if(tuning)
-  {
-    tuning=false;
-    changeAutoTune();
-    tuning=true;
-  }
- 
+	pid->SetOutputLimits(-14096,14095);
+	pid->SetSampleTime(10);  //10ms
+	pid->SetMode(AUTOMATIC);
+	//  pid.SetControllerDirection(REVERSE );
+	if(tuning)
+	{
+		tuning=false;
+		changeAutoTune();
+		tuning=true;
+	}
+
 
 }
 
@@ -195,134 +210,135 @@ pid->SetOutputLimits(-14096,14095);
 
 void CMDMOTOR::stop()
 {
-  newPoint=point=0;
-  Input=Output=0;
-  enabled=false;
-  setPWMValue(0);
-  #if ENABLE_SPEED
-    getEncoder()->resetSpeed();
-  #endif
-  getPID()->Initialize();
+	newPoint=point=0;
+	Input=Output=0;
+	enabled=false;
+	setPWMValue(0);
+#if ENABLE_SPEED
+	if (getEncoder()!=0)
+		getEncoder()->resetSpeed();
+#endif
+	getPID()->Initialize();
 }
 /* voltage apply on motor */
 void CMDMOTOR::setPWMValue(signed int m1) {
-  //cut it only if direction change to prevent from speak during polaryty change on motor
-  Output = m1;
+	//cut it only if direction change to prevent from speak during polaryty change on motor
+	Output = m1;
 
-  m1 = (m1 > PWMMAX) ? PWMMAX : m1;
-  m1 = (m1 < -PWMMAX) ? -PWMMAX : m1;
-  if (m1 == 0) {
-//       noInterrupts();
-    digitalWrite(pinMM, LOW);
-    digitalWrite(pinMP, LOW);
-//     interrupts();
-  if (SerialDebug) SerialDebug->print("Mx= STOP,");
-  } else if (m1 > 0) {
-//      noInterrupts();
-    digitalWrite(pinMM, LOW);
-    analogWrite(pinMP, m1);
-  //   interrupts();
-    if (SerialDebug) 
-    {
-    SerialDebug->print("Mx= LOW,");
-    SerialDebug->print(m1);
-    SerialDebug->print(" \r\n");
-    }
-  } else {
-   //  noInterrupts();
-    digitalWrite(pinMM, HIGH);
-    analogWrite(pinMP, PWMMAX + m1);
-  //   interrupts();
-    if (SerialDebug) 
-    {
-    SerialDebug->print("Mx= HIGH,");
-    SerialDebug->print(m1);
-    SerialDebug->print(" \r\n");
-    }
-  }
+	m1 = (m1 > PWMMAX) ? PWMMAX : m1;
+	m1 = (m1 < -PWMMAX) ? -PWMMAX : m1;
+	if (m1 == 0) {
+		//       noInterrupts();
+		digitalWrite(pinMM, LOW);
+		digitalWrite(pinMP, LOW);
+		//     interrupts();
+		if (SerialDebug) SerialDebug->print("Mx= STOP,");
+	} else if (m1 > 0) {
+		//      noInterrupts();
+		digitalWrite(pinMM, LOW);
+		analogWrite(pinMP, m1);
+		//   interrupts();
+		if (SerialDebug) 
+		{
+			SerialDebug->print("Mx= LOW,");
+			SerialDebug->print(m1);
+			SerialDebug->print(" \r\n");
+		}
+	} else {
+		//  noInterrupts();
+		digitalWrite(pinMM, HIGH);
+		analogWrite(pinMP, PWMMAX + m1);
+		//   interrupts();
+		if (SerialDebug) 
+		{
+			SerialDebug->print("Mx= HIGH,");
+			SerialDebug->print(m1);
+			SerialDebug->print(" \r\n");
+		}
+	}
 
 }
 
 void CMDMOTOR::loop() {
-  
+
 #ifdef ROS_USED 
-  getEncoder()->loop();
+	getEncoder()->loop();
 #endif
-  
-  if (enabled)
-  {
- #ifdef SMOOTH
-  if (newPoint != point) {
-    if ((newPoint - point) > 0) {
-      point = point + MIN(dpointMax, (newPoint - point));
-     /* SerialDebug->print(", MIN(dpointMax,(newPoint-point)=: ");
+
+	if (enabled)
+	{
+#ifdef SMOOTH
+		if (newPoint != point) {
+			if ((newPoint - point) > 0) {
+				point = point + MIN(dpointMax, (newPoint - point));
+				/* SerialDebug->print(", MIN(dpointMax,(newPoint-point)=: ");
       SerialDebug->print(MIN(dpointMax, (newPoint - point)));*/
-    } else {
-      point = point - MIN(dpointMax, (point - newPoint));
-    /*  SerialDebug->print(", -MIN(dpointMax,(point-newPoint)=: ");
+			} else {
+				point = point - MIN(dpointMax, (point - newPoint));
+				/*  SerialDebug->print(", -MIN(dpointMax,(point-newPoint)=: ");
       SerialDebug->print(MIN(dpointMax, (point - newPoint)));*/
-    }
-  }
+			}
+		}
 #endif
-  
-  /*
+
+		/*
   SerialDebug->print(", point=: ");
   SerialDebug->print(point);
   SerialDebug->print("\r\n");*/
- // Input = getEncoder()->getValue();
- #if ENABLE_SPEED
+		// Input = getEncoder()->getValue();
+#if ENABLE_SPEED
 
-  int speed = getEncoder()->getSpeed();
+		int speed = getEncoder()->getSpeed();
 #else
-   int speed = getEncoder()->getDeltaValue();
+		int speed = getEncoder()->getDeltaValue();
 #endif
-  Input = speed;
+		Input = speed;
 
-  
-   int res =0;
 
-  
-   if(tuning)
-  {
-    byte val = (aTune->Runtime());
-    if (val!=0)
-    {
-      tuning = false;
-    }
-    if(!tuning)
-    { //we're done, set the tuning parameters
-      Kp = aTune->GetKp();
-      Ki = aTune->GetKi();
-      Kd = aTune->GetKd();
-      pid->SetTunings(Kp,Ki,Kd);
-      AutoTuneHelper(false);
-    }
-  }
-else
-   res = pid->Compute();
+		int res =0;
 
-  /*  SerialDebug->print(", newPID=: ");  SerialDebug->print(res);*/
-  if(res)// if refresh
-    setPWMValue((signed int)Output);
+
+		if(tuning)
+		{
+			byte val = (aTune->Runtime());
+			if (val!=0)
+			{
+				tuning = false;
+			}
+			if(!tuning)
+			{ //we're done, set the tuning parameters
+				Kp = aTune->GetKp();
+				Ki = aTune->GetKi();
+				Kd = aTune->GetKd();
+				pid->SetTunings(Kp,Ki,Kd);
+				AutoTuneHelper(false);
+			}
+		}
+		else
+			res = pid->Compute();
+
+		/*  SerialDebug->print(", newPID=: ");  SerialDebug->print(res);*/
+		if(res)// if refresh
+			setPWMValue((signed int)Output);
 
 #ifdef PcomPlotter
 
-  PcomPlotter.print(point);
-  PcomPlotter.print("\t");
-  PcomPlotter.print(Input);
+		PcomPlotter.print(point);
+		PcomPlotter.print("\t");
+		PcomPlotter.print(Input);
 
-  PcomPlotter.print("\t");
-  PcomPlotter.print(Output);
-  PcomPlotter.print("\t");
+		PcomPlotter.print("\t");
+		PcomPlotter.print(Output);
+		PcomPlotter.print("\t");
 #endif
-  /*
+		/*
    SerialDebug->print(", spd:");
    SerialDebug->print(speed);
-   
+
    SerialDebug->print(", ms:");
    SerialDebug->print(millis());*/
-  /*
-   
+		/*
+
    SerialDebug->print(", Kpid: ");
    SerialDebug->print(pid.GetKp());SerialDebug->print(",");
    SerialDebug->print(pid.GetKi());SerialDebug->print(",");
@@ -332,9 +348,9 @@ else
 
    SerialDebug->print(", GetMode: ");
    SerialDebug->print(pid.GetMode());SerialDebug->print(",");*/
-  
-  
-}
+
+
+	}
 }
 
 
@@ -345,120 +361,120 @@ else
 CMDMOTOR * myZcmdmotor[8];
 static void callbackinstancepwm0( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[0]->enabled=false;
-  myZcmdmotor[0]->setPWMValue(-pwm_msg.data);  
+	myZcmdmotor[0]->enabled=false;
+	myZcmdmotor[0]->setPWMValue(-pwm_msg.data);  
 }
 
 static void callbackinstancespeed0( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[0]->setPoint(-speed_msg.data);  
+	myZcmdmotor[0]->setPoint(-speed_msg.data);  
 
 }
 
 static void callbackinstancepwm1( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[1]->enabled=false;
-  myZcmdmotor[1]->setPWMValue(-pwm_msg.data);  
+	myZcmdmotor[1]->enabled=false;
+	myZcmdmotor[1]->setPWMValue(-pwm_msg.data);  
 }
 
 static void callbackinstancespeed1( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[1]->setPoint(-speed_msg.data);    
+	myZcmdmotor[1]->setPoint(-speed_msg.data);    
 }
 
 static void callbackinstancepwm2( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[2]->enabled=false;
-  myZcmdmotor[2]->setPWMValue(-pwm_msg.data);  
- }
+	myZcmdmotor[2]->enabled=false;
+	myZcmdmotor[2]->setPWMValue(-pwm_msg.data);  
+}
 
 static void callbackinstancespeed2( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[2]->setPoint(-speed_msg.data);    
+	myZcmdmotor[2]->setPoint(-speed_msg.data);    
 }
 static void callbackinstancepwm3( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[3]->enabled=false;
-  myZcmdmotor[3]->setPWMValue(-pwm_msg.data);    
+	myZcmdmotor[3]->enabled=false;
+	myZcmdmotor[3]->setPWMValue(-pwm_msg.data);    
 }
 
 static void callbackinstancespeed3( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[3]->setPoint(speed_msg.data);    
+	myZcmdmotor[3]->setPoint(speed_msg.data);    
 }
 
 static void callbackinstancepwm4( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[4]->enabled=false;
-  myZcmdmotor[4]->setPWMValue(-pwm_msg.data);    
+	myZcmdmotor[4]->enabled=false;
+	myZcmdmotor[4]->setPWMValue(-pwm_msg.data);    
 }
 
 static void callbackinstancespeed4( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[4]->setPoint(speed_msg.data);    
+	myZcmdmotor[4]->setPoint(speed_msg.data);    
 }
 
 static void callbackinstancepwm5( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[5]->enabled=false;
-  myZcmdmotor[5]->setPWMValue(-pwm_msg.data);    
+	myZcmdmotor[5]->enabled=false;
+	myZcmdmotor[5]->setPWMValue(-pwm_msg.data);    
 }
 
 static void callbackinstancespeed5( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[5]->setPoint(speed_msg.data);    
+	myZcmdmotor[5]->setPoint(speed_msg.data);    
 }
 
 static void callbackinstancepwm6( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[6]->enabled=false;
-  myZcmdmotor[6]->setPWMValue(-pwm_msg.data);    
+	myZcmdmotor[6]->enabled=false;
+	myZcmdmotor[6]->setPWMValue(-pwm_msg.data);    
 }
 
 static void callbackinstancespeed6( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[6]->setPoint(speed_msg.data);    
+	myZcmdmotor[6]->setPoint(speed_msg.data);    
 }
 
 static void callbackinstancepwm7( const std_msgs::Int16& pwm_msg)
 {	
-  myZcmdmotor[7]->enabled=false;
-  myZcmdmotor[7]->setPWMValue(-pwm_msg.data);    
+	myZcmdmotor[7]->enabled=false;
+	myZcmdmotor[7]->setPWMValue(-pwm_msg.data);    
 }
 
 static void callbackinstancespeed7( const std_msgs::Int32& speed_msg)
 {	
-  myZcmdmotor[7]->setPoint(speed_msg.data);    
+	myZcmdmotor[7]->setPoint(speed_msg.data);    
 }
 
 static void(*callbackinstancespeed[8])(const std_msgs::Int32& cmd_msg)={
-	callbackinstancespeed0,callbackinstancespeed1,callbackinstancespeed2,callbackinstancespeed3,
-	callbackinstancespeed4,callbackinstancespeed5,callbackinstancespeed6,callbackinstancespeed7	
-	};
+		callbackinstancespeed0,callbackinstancespeed1,callbackinstancespeed2,callbackinstancespeed3,
+		callbackinstancespeed4,callbackinstancespeed5,callbackinstancespeed6,callbackinstancespeed7	
+};
 static void(*callbackinstancepwm[8])(const std_msgs::Int16& cmd_msg)={
-	callbackinstancepwm0,callbackinstancepwm1,callbackinstancepwm2,callbackinstancepwm3,
-	callbackinstancepwm4,callbackinstancepwm5,callbackinstancepwm6,callbackinstancepwm7	
-	};
+		callbackinstancepwm0,callbackinstancepwm1,callbackinstancepwm2,callbackinstancepwm3,
+		callbackinstancepwm4,callbackinstancepwm5,callbackinstancepwm6,callbackinstancepwm7	
+};
 static  int index=0;
 /** setup :
   At setup after NodeHandle setup, call this to initialise the topic
-*/
+ */
 void CMDMOTOR::setup( ros::NodeHandle * myNodeHandle,	const char   *	topicPWM,	const char   *	topicSPEED)
 {
-  assert(index<8);
-  myZcmdmotor[index]= this;
-  nh=myNodeHandle;
-  subPWM=new ros::Subscriber<std_msgs::Int16> (topicPWM, callbackinstancepwm[index]); 
-  assert(subPWM!=0);// heap issue.
-  subSpeed=new ros::Subscriber<std_msgs::Int32> (topicSPEED, callbackinstancespeed[index]); 
-  assert(subSpeed!=0);// heap issue.
-  nh->subscribe(*subPWM); 
-  nh->subscribe(*subSpeed); 
-  DEBUG(nh->loginfo("CMDMOTOR::setup()")); 
-  DEBUG(nh->loginfo(topicPWM)); 
-  DEBUG(nh->loginfo(topicSPEED)); 
-  index++;
-  
+	assert(index<8);
+	myZcmdmotor[index]= this;
+	nh=myNodeHandle;
+	subPWM=new ros::Subscriber<std_msgs::Int16> (topicPWM, callbackinstancepwm[index]); 
+	assert(subPWM!=0);// heap issue.
+	subSpeed=new ros::Subscriber<std_msgs::Int32> (topicSPEED, callbackinstancespeed[index]); 
+	assert(subSpeed!=0);// heap issue.
+	nh->subscribe(*subPWM); 
+	nh->subscribe(*subSpeed); 
+	DEBUG(nh->loginfo("CMDMOTOR::setup()")); 
+	DEBUG(nh->loginfo(topicPWM)); 
+	DEBUG(nh->loginfo(topicSPEED)); 
+	index++;
+
 }
 
 
